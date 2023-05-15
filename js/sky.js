f15fc59f21d07b5e30941dac0b0dc05b89578d1f
Dawn-of-Time <1733442166@qq.com>
2023-05-15 17:48:24 +0800

    Site updated: 2023-05-15 17:48:24

--- js/sky.js
+++ js/sky.js
@@ -1,101 +1,59 @@
-var scene,
-    camera,
-    renderer,
-    container,
-    aspect,
-    fov,
-    plane,
-    far,
-    mouseX,
-    mouseY,
-    windowHalfX,
-    windowHalfY,
-    geometry,
-    starStuff,
-    materialOptions,
-    stars;
-
-init();
-animate();
-
 function init() {
-    container = document.getElementById(`canvas`);
-
-    mouseX = 0;
-    mouseY = 0;
-
-    aspect = window.innerWidth / window.innerHeight;
-    fov = 40;
-    plane = 1;
-    far = 800;
-
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
-
-    camera = new THREE.PerspectiveCamera(
-        fov,
-        aspect,
-        plane,
-        far
-    );
-
-    camera.position.z = far / 2;
-
-    scene = new THREE.Scene({ antialias: true });
-    scene.fog = new THREE.FogExp2(0x1b1b1b, 0.0001);
-
-    starForge();
-
-    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.autoClear = false;
-    renderer.setClearColor(0x000000, 0.0);
-    container.appendChild(renderer.domElement);
-
-    document.addEventListener('mousemove', onMouseMove, false);
+    container = document.getElementById("canvas"),
+    mouseY = mouseX = 0,
+    aspect = window.innerWidth / window.innerHeight,
+    fov = 40,
+    plane = 1,
+    far = 800,
+    windowHalfX = window.innerWidth / 2,
+    windowHalfY = window.innerHeight / 2,
+    (camera = new THREE.PerspectiveCamera(fov,aspect,plane,far)).position.z = far / 2,
+    (scene = new THREE.Scene({
+        antialias: !0
+    })).fog = new THREE.FogExp2(1776411,1e-4),
+    starForge(),
+    (renderer = new THREE.WebGLRenderer({
+        antialias: !0,
+        alpha: !0
+    })).setPixelRatio(window.devicePixelRatio || 1),
+    renderer.setSize(window.innerWidth, window.innerHeight),
+    renderer.autoClear = !1,
+    renderer.setClearColor(0, 0),
+    container.appendChild(renderer.domElement),
+    document.addEventListener("mousemove", onMouseMove, !1)
 }
-
 function animate() {
-    requestAnimationFrame(animate);
-    render();
+    requestAnimationFrame(animate),
+    render()
 }
-
 function render() {
-    camera.position.x += (mouseX - camera.position.x) * 0.005;
-    camera.position.y += (-mouseY - camera.position.y) * 0.005;
-    camera.lookAt(scene.position);
-    renderer.render(scene, camera);
+    camera.position.x += .005 * (mouseX - camera.position.x),
+    camera.position.y += .005 * (-mouseY - camera.position.y),
+    camera.lookAt(scene.position),
+    renderer.render(scene, camera)
 }
-
 function starForge() {
-    var amount = 45000;
-    geometry = new THREE.SphereGeometry(1000, 100, 50);
-
+    geometry = new THREE.SphereGeometry(1e3,100,50),
     materialOptions = {
-        color: new THREE.Color(0xffffff),
+        color: new THREE.Color(16777215),
         size: 1.1,
-        transparent: true,
-        opacity: 0.8
-    };
-
+        opacity: .8
+    },
     starStuff = new THREE.PointsMaterial(materialOptions);
-
-
-    for (var i = 0; i < amount; i++) {
-        var item = new THREE.Vector3();
-        item.x = Math.random() * 2000 - 1000;
-        item.y = Math.random() * 2000 - 1000;
-        item.z = Math.random() * 2000 - 1000;
-
-        geometry.vertices.push(item.x, item.y, item.z);
+    for (var e = 0; e < 45e3; e++) {
+        var n = new THREE.Vector3;
+        n.x = 2e3 * Math.random() - 1e3,
+        n.y = 2e3 * Math.random() - 1e3,
+        n.z = 2e3 * Math.random() - 1e3,
+        geometry.vertices.push(n)
     }
-
-    stars = new THREE.Points(geometry, starStuff);
-    scene.add(stars);
+    stars = new THREE.Points(geometry,starStuff),
+    scene.add(stars)
 }
-
 function onMouseMove(e) {
-    mouseX = e.clientX - windowHalfX;
-    mouseY = e.clientY - windowHalfY;
+    mouseX = e.clientX - windowHalfX,
+    mouseY = e.clientY - windowHalfY
 }
+var scene, camera, renderer, container, aspect, fov, plane, far, mouseX, mouseY, windowHalfX, windowHalfY, geometry, starStuff, materialOptions, stars;
+void 0 === container && (init(),
+animate());
